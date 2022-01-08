80a6ed0eb12bc4faf597d304873003fad3acb062
iantalent <iantalent95@gmail.com>
2022-01-09 00:45:30 +0200

    optimize

--- lib/type.ts
+++ lib/type.ts
@@ -1,11 +1,6 @@
 export type TypeOrFunction<T> = T | (() => T);
 
-function isFunction(functionPretender: any): functionPretender is Function
-{
-	return functionPretender instanceof Function;
-}
-
 export function getTypeOrFunctionValue<T>(value: TypeOrFunction<T>): T
 {
-	return isFunction(value) ? value() : value;
+	return value instanceof Function ? value() : value;
 }
