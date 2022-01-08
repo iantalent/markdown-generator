2c573b82e5883350fa9f3df36bbf711c30c9e842
iantalent <iantalent95@gmail.com>
2022-01-09 00:43:09 +0200

    simplify

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import {Page} from "./page";
 import {FragmentsContainer, InlineFragmentsContainer, SimpleFragmentsContainer} from "./container";
+import {getTypeOrFunctionValue} from "./type";
 
 export function isFragmentsContainer(pretender: any): pretender is FragmentsContainer
 {
@@ -16,30 +17,21 @@ export function isPage(pretender: any): pretender is Page
 
 /**
  * @param {FragmentsContainer} container
- * @param {string} separator
  * @returns {string}
  */
-export function buildMarkdown(container: FragmentsContainer, separator: string = "\r\n"): string
+export function buildMarkdown(container: FragmentsContainer): string
 {
 	return container.tree().map(entry =>
 	{
 		if(typeof entry === 'string')
 			return entry;
 		else if(Array.isArray(entry))
-			return buildMarkdown((new SimpleFragmentsContainer()).add(entry), separator);
+			return buildMarkdown((new SimpleFragmentsContainer()).add(entry));
 		else if(isFragmentsContainer(entry)) //FragmentsContainer
-			return buildMarkdown(entry, separator);
+			return buildMarkdown(entry);
 		else if(entry['content'])
-		{
-			if(typeof entry['content'] === 'function')
-				return entry.content();
-			
-			if(typeof entry['content'] === 'string')
-				return entry.content;
-			
-			throw new Error('There is wrong content type in SimpleFragment. Allowed only function and string. Got ' + typeof entry['content']);
-		}
+			return getTypeOrFunctionValue(entry.content);
 		else
 			throw new Error('There is wrong item in container. Allowed only Fragment, FragmentsContainer, string');
-	}).filter(value => value).join(container instanceof InlineFragmentsContainer ? '' : separator)
+	}).filter(value => value).join(getTypeOrFunctionValue(container.separator))
 }
