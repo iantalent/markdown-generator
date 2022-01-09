2a1035159cc9c19cb4515655c310bd230ec994d6
iantalent <iantalent95@gmail.com>
2022-01-09 22:43:17 +0200

    progress

--- lib/index.ts
+++ lib/index.ts
@@ -3,6 +3,4 @@ export * from './fragment/vuepress'
 export * from './container'
 export * from './page'
 
-import plugin from './plugin';
-
-export default plugin;
+export {default} from './plugin';
