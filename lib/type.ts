3b6f1c902c6fe10ae70605e07c8a0af3635c14f5
iantalent <iantalent95@gmail.com>
2022-01-16 21:13:46 +0200

    add thisArg parameter

--- lib/type.ts
+++ lib/type.ts
@@ -1,6 +1,6 @@
 export type TypeOrFunction<T> = T | (() => T);
 
-export function getTypeOrFunctionValue<T>(value: TypeOrFunction<T>): T
+export function getTypeOrFunctionValue<T>(value: TypeOrFunction<T>, thisArg?: any): T
 {
-	return value instanceof Function ? value() : value;
+	return value instanceof Function ? value.apply(thisArg) : value;
 }
