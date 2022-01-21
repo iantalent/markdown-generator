897e1eb134a76a8739ec79a23be2e9d2df401e54
sea <iantalent95@gmail.com>
2022-01-21 14:56:54 +0200

    Fix

--- lib/fragment/vuepress.ts
+++ lib/fragment/vuepress.ts
@@ -6,6 +6,6 @@ export class Tip extends WrappedNewLineFragment
 {
 	constructor(content: WrappedFragmentContent, title: string = '')
 	{
-		super(content, '::: tip ' + (title || ''), ':::')
+		super(content, '::: tip' + (title ? (' ' + title) : ''), ':::')
 	}
 }
