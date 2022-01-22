0567b646fcc0dca9bef19726c317c0da06d23433
sea <iantalent95@gmail.com>
2022-01-22 12:55:03 +0200

    support changed Fragmentcontent

--- lib/fragment/vuepress.ts
+++ lib/fragment/vuepress.ts
@@ -1,10 +1,10 @@
-import {WrappedFragmentContent, WrappedNewLineFragment} from "./common";
-import {blockLevelFragment} from "../fragment";
+import {WrappedNewLineFragment} from "./common";
+import {blockLevelFragment, FragmentContent} from "../fragment";
 
 @blockLevelFragment
 export class Tip extends WrappedNewLineFragment
 {
-	constructor(content: WrappedFragmentContent, title: string = '')
+	constructor(content: FragmentContent, title: string = '')
 	{
 		super(content, '::: tip' + (title ? (' ' + title) : ''), ':::')
 	}
