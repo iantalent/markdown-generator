5a7b6db9a4b62a620d2affd73c1ca1fe2f460389
sea <iantalent95@gmail.com>
2022-01-22 12:55:12 +0200

    update

---
+++ tests/pageFragments.test.ts
@@ -0,0 +1,41 @@
+import {suite, test} from '@testdeck/mocha';
+import * as chai from 'chai';
+import {Bold, Code, Italic, Paragraph, SimpleFragment, SimplePage, Tip,} from "../lib";
+import {buildMarkdown} from "../lib/utils";
+
+const assert = chai.assert;
+
+@suite class pageFragments {
+	
+	before() {
+	
+	}
+	
+	@test 'Simple Page'()
+	{
+		const page = new SimplePage("title", "/path/");
+		page.add(
+			new SimpleFragment('simple fragment'),
+			new Paragraph(['Paragraph 1 ', new Bold('bold')]),
+			new Paragraph(new Italic('Italic text')),
+			new Paragraph(new Code('Boolean')),
+			new Tip('You should go this way not that!'),
+			new Paragraph('Some paragraph info')
+		);
+		assert.equal(buildMarkdown(page), '# title\r\n\r\n' +
+			'simple fragment\r\n\r\n' +
+			'Paragraph 1 **bold**\r\n\r\n' +
+			'*Italic text*\r\n\r\n' +
+			'`Boolean`\r\n\r\n' +
+			'::: tip\r\n\r\nYou should go this way not that!\r\n\r\n:::\r\n\r\n' +
+			'Some paragraph info');
+	}
+	
+	@test 'VuePress Builtin fragments'()
+	{
+		assert.equal(buildMarkdown([new Tip('Tip content')]), '::: tip\r\n\r\nTip content\r\n\r\n:::');
+	}
+	
+	
+	
+}
