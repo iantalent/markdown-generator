c8fd5e45f62401b3542b46ede5eef5bd2a3f75a3
sea <iantalent95@gmail.com>
2022-01-25 11:14:39 +0200

    almost ready. We need use prefixes and indent for lines

--- lib/builder.ts
+++ lib/builder.ts
@@ -87,6 +87,11 @@ class FragmentResult
 	}
 }
 
+type builderState = {
+	isFirstLine: boolean,
+	requireBlankLine: boolean
+};
+
 export class MarkdownBuilder
 {
 	
@@ -106,10 +111,38 @@ export class MarkdownBuilder
 	
 	private build(): string
 	{
-		return this.buildContainer(this.rootContainer);
+		return this.buildFragmentsResults(
+			this.buildContainerFragments(this.rootContainer)
+		);
+		//return this.buildContainer(this.rootContainer);
 	}
 	
-	private buildContainerNew(container: Container): Array<FragmentResult>
+	private buildFragmentsResults(results: Array<FragmentResult>, state: builderState = {
+		isFirstLine: true,
+		requireBlankLine: false
+	}): string
+	{
+		return results.map(entry =>
+		{
+			let result = entry.content,
+				resultPrefixes = '';
+			
+			if(entry.blockLevel && !state.isFirstLine)
+				resultPrefixes += '\r\n\r\n';
+			
+			if(entry.prefixes.length)
+				resultPrefixes += entry.prefixes.join('') + ' ';
+			
+			state.isFirstLine = false;
+			
+			if(entry.results.length)
+				result += this.buildFragmentsResults(entry.results, state);
+			
+			return resultPrefixes + result;
+		}).join('');
+	}
+	
+	private buildContainerFragments(container: Container): Array<FragmentResult>
 	{
 		const results: Array<FragmentResult> = [];
 		
@@ -124,9 +157,7 @@ export class MarkdownBuilder
 			if(typeof entry === 'string')
 				result = new FragmentResult(entry);
 			else if(Array.isArray(entry))
-			{
-				result = new FragmentResult(this.buildContainerNew(entry));
-			}
+				result = new FragmentResult(this.buildContainerFragments(entry));
 			else if(isFragment(entry))
 			{
 				const isBlockLevel = isBlockLevelFragment(entry);
@@ -138,10 +169,12 @@ export class MarkdownBuilder
 				}
 				result = new FragmentResult(this.buildFragment(entry));
 				result.blockLevel = isBlockLevel;
-				
 			}
 			else
 				throw new Error('There is wrong item in container. Allowed only Fragment, FragmentsContainer, string. Got ' + typeof entry);
+			
+			if(this.linePrefixes.length)
+				result.prefixes = [...this.linePrefixes];
 			
 			results.push(result);
 			
@@ -153,181 +186,32 @@ export class MarkdownBuilder
 	
 	private buildFragment(fragment: Fragment): Array<FragmentResult>
 	{
-		return [];
-	}
-	
-	private buildContainer(container: Container): string
-	{
-		const lines = this.buildLines(container);
-		let content = '';
-		if(lines.length)
-		{
-			for(let index = 0; index < lines.length; index++)
-			{
-				const line = lines[index];
-				if(line.needBlankLineBefore && index > 0 && !lines[index - 1].isBlank)
-					content += '\r\n';
-				
-				let lineContent = line.content;
-				
-				if(line.indent)
-					lineContent = '\t'.repeat(line.indent) + lineContent;
-				
-				if(line.prefixes.length)
-					lineContent = line.prefixes.join('') + ' ' + lineContent;
-				
-				if(line.needBlankLineAfter && index + 1 < lines.length && !lines[index + 1].isBlank)
-					content += '\r\n';
-			}
-		}
-		return content;
-	}
-	
-	private buildLines(container: Container): Array<MarkdownLine>
-	{
-		const separator = isSeparatedFragmentsContainer(container) ?
-			getTypeOrFunctionValue(container.separator, container) : '';
-		
-		if(Array.isArray(container))
-			container = createContainerFromArray(container);
-		
-		const lines: Array<MarkdownLine> = [];
-		
-		container.tree().forEach(entry =>
-		{
-			if(typeof entry === 'string')
-				return entry;
-			else if(Array.isArray(entry))
-				mergeLines(lines, this.buildLines((new SimpleFragmentsContainer()).add(entry)));
-			else if(isFragmentsContainer(entry))
-				mergeLines(lines, this.buildLines(entry));
-			else if(isFragment(entry))
-			{
-				mergeLines(lines, this.getFragmentLines(entry));
-				/*const isBlockLevel = isBlockLevelFragment(entry) && getTypeOrFunctionValue(entry.blockLevel, entry),
-					before = !this.isFirstFragment && (isBlockLevel || this.requiredBlankLines) ? '\r\n\r\n' : '',
-					isLinePrefix = isLinePrefixFragment(entry);
-				
-				if(isLinePrefix)
-					this.linePrefixes.push(getTypeOrFunctionValue(entry.linePrefix, entry));
-				
-				let entryLines = this.getFragmentLines(entry);
-				
-				this.isFirstFragment = false;
-				this.requiredBlankLines = isBlockLevel;
-				
-				if(this.linePrefixes.length)
-					result = this.prefixLines(result);
-				
-				if(isLinePrefix)
-					this.linePrefixes.pop();
-				
-				return entryLines;*/
-			}
-			else
-				throw new Error('There is wrong item in container. Allowed only Fragment, FragmentsContainer, string. Got ' + typeof entry);
-		});
-		
-		//console.log(lines);
-		
-		return lines;
-	}
-	
-	getFragmentLines(fragment: Fragment): Array<MarkdownLine>
-	{
 		const content = getTypeOrFunctionValue(fragment.content, fragment);
-		let lines;
-		
 		if(typeof content === 'string')
-			lines = buildLinesFromContent(content);
+			return this.buildContainerFragments([content]);
 		else if(Array.isArray(content))
-			lines = this.getArrayContentLines(content);
+			return this.buildFragmentArrayContent(content);
 		else if(isFragment(content))
-			lines = this.getFragmentLines(content);
-		else
-			throw new Error('Fragment content can be only string, fragment or Array of strings or fragments');
-		
-		if(lines.length)
-		{
-			let isBlockLevel = isBlockLevelFragment(fragment),
-				prefix = '';
-			
-			if(isBlockLevel)
-			{
-				lines[0].needBlankLineBefore = true;
-				lines[lines.length - 1].needBlankLineAfter = true;
-				
-			}
-			if(isLinePrefixFragment(fragment))
-				prefix = getTypeOrFunctionValue(fragment.linePrefix, fragment);
-			
-			if(isBlockLevel || prefix)
-			{
-				lines.forEach(line =>
-				{
-					if(isBlockLevel)
-						line.blockLevelLine = true;
-					
-					if(prefix)
-						line.prefixes.push(prefix);
-				})
-			}
-		}
-		return lines;
-	}
-	
-	getArrayContentLines(contentArray: Array<FragmentContent>): Array<MarkdownLine>
-	{
-		const lines: Array<MarkdownLine> = [];
-		for(const contentItem of contentArray)
-		{
-			if(typeof contentItem === 'string')
-				mergeLines(lines, buildLinesFromContent(contentItem));
-			else if(Array.isArray(contentItem))
-				mergeLines(lines, this.getArrayContentLines(contentItem));
-			else if(isFragment(contentItem))
-				mergeLines(lines, this.getFragmentLines(contentItem));
-			else
-				throw new Error('Content array can store only strings, fragments or another content arrays');
-		}
-		console.log('array lines', lines);
-		return lines;
-	}
-	
-	prefixLines(content: string): string
-	{
-		const prefixes = this.linePrefixes.join('');
-		return content.split('\r\n').map(line => prefixes + ' ' + line).join('\r\n');
-	}
-	
-	processFragmentArrayContent(contentArray: Array<FragmentContent>): string
-	{
-		const parts: Array<string> = [];
-		for(const contentItem of contentArray)
-		{
-			if(typeof contentItem === 'string')
-				parts.push(contentItem);
-			else if(Array.isArray(contentItem))
-				parts.push(this.processFragmentArrayContent(contentItem));
-			else if(isFragment(contentItem))
-				parts.push(this.processFragmentContent(contentItem));
-		}
-		return parts.join('');
-	}
-	
-	processFragmentContent(fragment: Fragment): string
-	{
-		const content = getTypeOrFunctionValue(fragment.content, fragment);
-		
-		if(typeof content === 'string')
-			return content;
-		
-		if(Array.isArray(content))
-			return this.processFragmentArrayContent(content);
-		
-		if(isFragment(content))
-			return this.processFragmentContent(content);
+			return this.buildFragment(content);
 		
 		throw new Error('Fragment content can be only string, fragment or Array of strings or fragments');
+	}
+	
+	private buildFragmentArrayContent(content: Array<FragmentContent>): Array<FragmentResult>
+	{
+		const results: Array<FragmentResult> = [];
+		if(content.length)
+		{
+			for(const contentItem of content)
+			{
+				if(typeof contentItem === 'string')
+					results.push(...this.buildContainerFragments([contentItem]));
+				else if(Array.isArray(contentItem))
+					results.push(...this.buildFragmentArrayContent(contentItem));
+				else if(isFragment(contentItem))
+					results.push(...this.buildContainerFragments([contentItem]));
+			}
+		}
+		return results;
 	}
 }
